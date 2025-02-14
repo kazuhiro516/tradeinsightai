1e7ed57c76778c0658ed0b949b216e66ae54e18e
kazuhiro_kurokawa <k.kurokawa11@gmail.com>
2025-02-14 22:35:15 +0900

    update: ビルドできるようにする

---
+++ app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <h2 className="text-2xl font-bold">ページが見つかりません</h2>
+      <p className="mt-4">申し訳ありませんが、お探しのページは存在しません。</p>
+    </div>
+  );
+}
