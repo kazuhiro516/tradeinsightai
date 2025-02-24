413cf9ad5334e4e087876cb3f25490ad8fd319c0
kazuhiro_kurokawa <k.kurokawa11@gmail.com>
2025-02-24 20:45:51 +0900

    add: バックエンド追加

---
+++ server/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello, Go Dockerized!")
+}
+
+func main() {
+	fmt.Println("Server starting...")
+	http.HandleFunc("/", handler)
+	fmt.Println("Server started at :8080")
+	http.ListenAndServe(":8080", nil)
+}
